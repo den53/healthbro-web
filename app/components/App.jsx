2948cdbc45360dc0988de1fc55b94c3217d1d1d4
Dennis A. Seidel <mail@dennisseidel.de>
2016-04-22 07:36:29 +0200

    customize bootstrap and css

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,3 +1,7 @@
+//require custom CSS
+require('./App.css');
+
+
 var React = require('react');
 
 //Add bootstrap
@@ -8,75 +12,83 @@ var App = React.createClass({
   render: function() {
     return (
       <div>
-        <nav className="navbar navbar-inverse navbar-fixed-top">
-          <div className="container">
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
             <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span className="sr-only">Toggle navigation</span>
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
               </button>
-              <a className="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" className="navbar-collapse collapse">
-              <form className="navbar-form navbar-right">
-                <div className="form-group">
-                  <input type="text" placeholder="Email" className="form-control" /></div>
-                  <div className="form-group">
-                    <input type="password" placeholder="Password" className="form-control" /></div>
-                    <button type="submit" className="btn btn-success">Sign in</button>
-                  </form>
-                </div>
-              </div>
-            </nav>
-
-            <div className="jumbotron">
-              <div className="container">
-                <h1>Hello, world!</h1>
-                <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-                <p>
-                  <a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a>
-                </p>
-              </div>
+              <a className="navbar-brand" href="#">My Website</a>
             </div>
 
-            <div className="container">
-              <div className="row">
-                <div className="col-md-4">
-                  <h2>Heading</h2>
-                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                  </p>
-                  <p>
-                    <a className="btn btn-default" href="#" role="button">View details »</a>
-                  </p>
-                </div>
-                <div className="col-md-4">
-                  <h2>Heading</h2>
-                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                  </p>
-                  <p>
-                    <a className="btn btn-default" href="#" role="button">View details »</a>
-                  </p>
-                </div>
-                <div className="col-md-4">
-                  <h2>Heading</h2>
-                  <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                  <p>
-                    <a className="btn btn-default" href="#" role="button">View details »</a>
-                  </p>
-                </div>
-              </div>
-
-              <hr />
-
-                <footer>
-                  <p>© 2015 Company, Inc.</p>
-                </footer>
-              </div>
-              <span classNameName="_hsShareImage hsShareImage">&nbsp;</span>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                <li className="active">
+                  <a href="#">Home
+                    <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">About
+                  </a>
+                </li>
+                <li classNameName="dropdown">
+                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown
+                    <span className="caret"></span>
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a href="#">Action</a>
+                    </li>
+                    <li>
+                      <a href="#">Another action</a>
+                    </li>
+                    <li>
+                      <a href="#">Something else here</a>
+                    </li>
+                    <li role="separator" className="divider"></li>
+                    <li>
+                      <a href="#">Separated link</a>
+                    </li>
+                    <li role="separator" className="divider"></li>
+                    <li>
+                      <a href="#">One more separated link</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
           </div>
-);},
+        </nav>
+
+        <div className="jumbotron">
+          <div className="container">
+            <h1>Hello, world!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p>
+              <a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a>
+            </p>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            [MainContent]
+          </div>
+
+          <hr/>
+
+          <footer>
+            <p>© 2015 MyWebsite</p>
+          </footer>
+        </div>
+        <span classNameName="_hsShareImage hsShareImage">&nbsp;</span>
+      </div>
+    );
+  }
 });
 
 module.exports = App;
