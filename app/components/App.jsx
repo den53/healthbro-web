a9a53ca62a6e2129f3b62fc8d1b4f757b2d7590a
Dennis A. Seidel <mail@dennisseidel.de>
2016-04-22 08:12:45 +0200

    refactor jumbotron and css out

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,4 @@
 //require custom CSS
-require('./App.css');
-
 
 var React = require('react');
 
@@ -8,72 +6,17 @@ var React = require('react');
 var bootstrap = require('bootstrap');
 var bootstrapStyle = require("../../node_modules/bootstrap/dist/css/bootstrap.css");
 
+//Ad custom components
+var Navbar = require('./Navbar.jsx');
+var Jumbotron = require('./Jumbotron.jsx');
+
+
 var App = React.createClass({
   render: function() {
     return (
       <div>
-        <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <a className="navbar-brand" href="#">My Website</a>
-            </div>
-
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul className="nav navbar-nav">
-                <li className="active">
-                  <a href="#">Home
-                    <span className="sr-only">(current)</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">About
-                  </a>
-                </li>
-                <li classNameName="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown
-                    <span className="caret"></span>
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a href="#">Action</a>
-                    </li>
-                    <li>
-                      <a href="#">Another action</a>
-                    </li>
-                    <li>
-                      <a href="#">Something else here</a>
-                    </li>
-                    <li role="separator" className="divider"></li>
-                    <li>
-                      <a href="#">Separated link</a>
-                    </li>
-                    <li role="separator" className="divider"></li>
-                    <li>
-                      <a href="#">One more separated link</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div className="jumbotron">
-          <div className="container">
-            <h1>Hello, world!</h1>
-            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-            <p>
-              <a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a>
-            </p>
-          </div>
-        </div>
-
+        <Navbar />
+        <Jumbotron />
         <div className="container">
           <div className="row">
             [MainContent]
---
+++ app/components/Jumbotron.jsx
@@ -0,0 +1,22 @@
+//require custom CSS
+require('./Jumbotron.css');
+
+var React = require('react');
+
+var Jumbotron = React.createClass({
+  render: function(){
+    return(
+      <div className="jumbotron">
+        <div className="container">
+          <h1>Hello, world!</h1>
+          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+          <p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Jumbotron;
